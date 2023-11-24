4c139dbd12bf246721831b1e0544a42fb5ca14e4
otitamario <48613669+otitamario@users.noreply.github.com>
2023-11-24 16:52:19 -0400

    Create main.js

---
+++ frontend/js/scripts/main.js
@@ -0,0 +1,44 @@
+import {abi,endereco} from './contract.js'
+import {conectar} from './conect.js'
+
+conectar();
+
+var registerContract = web3.eth.contract(abi);
+var register = registerContract.at(endereco);
+var registerEvent = register.myUser();
+
+$("#button1").click(function(){
+    $("#loader").show();
+    register.registerUser(
+        $("#fNome").val(),$("#fIdade").val(),$("fCPF").val(),$("#fCargo").val(),$("#fHash").val(),function(error,result){
+            if(!error){
+                console.log("ok");
+                $("#loader").hide();
+                //o alerta aguarda até que esse evento seja emitido e o ative
+                registerEvent.watch(function(err,result){
+                    if(!err){
+                        alert("cadastro efetuado com sucesso");
+                    }
+                });
+            }
+            else {
+                console.error(error);
+                $("#loader").hide();
+                alert("cadastro não efetuado");
+            }
+        }
+    )
+});
+
+
+$("#button2").click(function(){
+
+    register.getUser($("#fID").val(),function(error,result){
+        if(!error){
+            $("#instructor").html(result);
+            console.log(result);
+        }
+        else 
+            console.error(error);
+    });
+});
